bb113c118af7a6944824ed73ef69e8928c61516a
Muhammed Bacalan <muhammedbacalan@gmail.com>
2023-07-19 21:57:46 +0200

    (server): Add websocket endpoints for MC server start/stop

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import Fastify from "fastify";
+import ws from '@fastify/websocket'
 import MC from "minecraft-protocol"
 import { spawn } from "node:child_process"
 
@@ -8,6 +9,8 @@ const fastify = Fastify({
   logger: true
 })
 
+await fastify.register(ws)
+
 fastify.get("/status", async function handler(_, reply) {
   try {
     const status = await MC.ping({ host: "192.168.178.251", port: "25565" })
@@ -16,61 +19,65 @@ fastify.get("/status", async function handler(_, reply) {
   } catch(err) {
     reply.code(200).send({ online: false, ...err })
   }
-
-  // const exampleStatus = {
-  //   version: { name: 'Paper 1.20.1', protocol: 763 },
-  //   enforcesSecureChat: true,
-  //   description: { text: 'A Minecraft Server' },
-  //   players: { max: 20, online: 0, sample: [{ id, name }]},
-  //   latency: 4
-  // }
 })
 
-fastify.post("/start", function handler(request, reply) {
-  mcServerProcess = spawn('java', [
-    "-Xms1G",
-    "-Xmx1G",
-    "-XX:+UseG1GC",
-    "-XX:+ParallelRefProcEnabled",
-    "-XX:MaxGCPauseMillis=200",
-    "-XX:+UnlockExperimentalVMOptions",
-    "-XX:+DisableExplicitGC",
-    "-XX:+AlwaysPreTouch",
-    "-XX:G1NewSizePercent=30",
-    "-XX:G1MaxNewSizePercent=40",
-    "-XX:G1HeapRegionSize=8M",
-    "-XX:G1ReservePercent=20",
-    "-XX:G1MixedGCCountTarget=4",
-    "-XX:InitiatingHeapOccupancyPercent=15",
-    "-XX:G1MixedGCLiveThresholdPercent=90",
-    "-XX:G1RSetUpdatingPauseTimePercent=5",
-    "-XX:G1HeapWastePercent=5",
-    "-XX:+PerfDisableSharedMem",
-    "-XX:MaxTenuringThreshold=1",
-    "-Dusing.aikars.flags=https://mcflags.emc.gs",
-    "-Daikars.new.flags=true",
-    "-jar paper.jar",
-    "--nogui",
-  ]);
+fastify.register(async function (fastify) {
+  fastify.get("/server", { websocket: true }, (connection, req) => {
+    connection.socket.on("message", async message => {
+      if (message == "start") {
+        mcServerProcess = spawn('java', [
+          "-Xms1G",
+          "-Xmx1G",
+          "-XX:+UseG1GC",
+          "-XX:+ParallelRefProcEnabled",
+          "-XX:MaxGCPauseMillis=200",
+          "-XX:+UnlockExperimentalVMOptions",
+          "-XX:+DisableExplicitGC",
+          "-XX:+AlwaysPreTouch",
+          "-XX:G1NewSizePercent=30",
+          "-XX:G1MaxNewSizePercent=40",
+          "-XX:G1HeapRegionSize=8M",
+          "-XX:G1ReservePercent=20",
+          "-XX:G1MixedGCCountTarget=4",
+          "-XX:InitiatingHeapOccupancyPercent=15",
+          "-XX:G1MixedGCLiveThresholdPercent=90",
+          "-XX:G1RSetUpdatingPauseTimePercent=5",
+          "-XX:G1HeapWastePercent=5",
+          "-XX:+PerfDisableSharedMem",
+          "-XX:MaxTenuringThreshold=1",
+          "-Dusing.aikars.flags=https://mcflags.emc.gs",
+          "-Daikars.new.flags=true",
+          "-jar",
+          "paper.jar",
+          "--nogui",
+        ], { cwd: '/home/mb/minecraft' });
 
-  mcServerProcess.once("spawn", () => {
-    reply.code(200).send({ success: true })
-  })
+        mcServerProcess.once("spawn", () => {
+          connection.socket.send(JSON.stringify({ message: "start", event: "spawn", success: true }))
+        })
 
-  mcServerProcess.on("error", (err) => {
-    console.log("error!", err)
-    reply.code(200).send({ success: false })
-  })
-})
+        mcServerProcess.on("error", (err) => {
+          connection.socket.send(JSON.stringify({ message: "start", event: "error", success: false, error: err }))
+        })
 
-fastify.post("/stop", function handler(_, reply) {
-  mcServerProcess.once("close", () => {
-    reply.code(200).send({ success: true })
+        mcServerProcess.stdout.on("data", (data) => {
+          connection.socket.send(JSON.stringify({ message: "start", event: "data", data: data.toString() }))
+        })
+      }
+
+      if (message == "stop") {
+        mcServerProcess.once("close", () => {
+          connection.socket.send(JSON.stringify({ message: "stop", event: "data", success: true }))
+        })
+
+        mcServerProcess.kill()
+      }
+    })
   })
 })
 
 try {
-  await fastify.listen({ port: 3000 })
+  await fastify.listen({ host: '192.168.178.251', port: 3000 })
 } catch (err) {
   fastify.log.error(err)
   process.exit(1)
