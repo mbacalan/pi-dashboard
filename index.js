3f591f60df19fda5a4b9bb98ab90b5124d73218a
Muhammed Bacalan <muhammedbacalan@gmail.com>
2023-07-19 22:00:25 +0200

    (client): Initial websocket usage for MC server start/stop

--- index.js
+++ index.js
@@ -4,12 +4,36 @@ const serverStatusText = document.getElementById("server-status-text")
 const serverStatusDetails = document.getElementById("server-status-details")
 const serverStatusVersion = document.getElementById("server-status-version")
 const serverStatusPlayers = document.getElementById("server-status-players")
+let wsOpen = false
+const ws = new WebSocket('ws://192.168.178.251:3000/server');
+
+ws.onopen = () => {
+  wsOpen = true
+};
 
 window.onload = async () => {
   serverStatusButton.addEventListener("click", () => checkServerStatus())
   serverStartButton.addEventListener("click", () => startServer())
 
   await checkServerStatus()
+
+  ws.onmessage = (event) => {
+    const eventData = JSON.parse(event.data)
+
+    if (eventData.message == "start" && eventData.event == "spawn") {
+      serverStatusDetails.removeAttribute("hidden")
+    }
+
+    if (eventData.message == "start" && eventData.event == "data") {
+      const node = document.createElement("p")
+      node.innerText = eventData.data
+      serverStatusLog.removeChild(serverStatusLog.firstChild)
+      serverStatusLog.appendChild(node)
+      node.scrollIntoView({ behavior: "smooth" })
+    }
+
+    console.log(eventData)
+  };
 }
 
 function handleServerStatus({ online, error }) {
@@ -42,7 +66,7 @@ async function checkServerStatus() {
   serverStatusText.setAttribute("aria-busy", true)
 
   try {
-    const response = await fetch("http://localhost:3000/status");
+    const response = await fetch("http://192.168.178.251:3000/status")
 
     if (!response.ok) {
       throw new Error("Network response was not OK");
@@ -64,7 +88,7 @@ async function checkServerStatus() {
 }
 
 function startServer() {
-  fetch("http://localhost:3000/start", {
-    method: "POST"
-  })
+  if (wsOpen) {
+    ws.send(serverRunning ? "stop" : "start")
+  }
 }
